3c6a2795bdfff3cca895f0afb0ccfcf3fe7652d4
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2020-06-06 02:15:03 -0500

    📦 ADD calidation in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import { errors } from "celebrate";
 
 import routes from "./routes";
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log("🔥: server on port 3333");
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
 
 import multer from "multer";
 import multerConfig from "./config/multerConfig";
@@ -10,7 +11,28 @@ const upload = multer(multerConfig);
 
 routes.get("/items", itemsController.index);
 
-routes.post("/points", upload.single("image"), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required(),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
